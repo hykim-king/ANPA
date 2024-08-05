fc23f2a92c951800a321ab5a74c038dbb8e874cc
M.S. Jang <ms3814@naver.com>
2024-08-05 17:45:38 +0900

    아무것도안함

--- src/main/java/com/acorn/anpa/code/service/CodeService.java
+++ src/main/java/com/acorn/anpa/code/service/CodeService.java
@@ -1,8 +1,28 @@
 package com.acorn.anpa.code.service;
 
+import java.sql.SQLException;
+import java.util.List;
+
+import com.acorn.anpa.cmn.Search;
 import com.acorn.anpa.cmn.WorkDiv;
 import com.acorn.anpa.code.domain.Code;
 
 public interface CodeService extends WorkDiv<Code>{
-
+	
+	/**
+	 * 코드 단건 조회
+	 * @param code(masterCode, subCode)
+	 * @return (bigList, midList)
+	 * @throws SQLException
+	 */
+	List<Code> doSelectCode(Code code) throws SQLException;
+	
+	/**
+	 * 10 -> 대분류 , 20 -> 소분류
+	 * @param search
+	 * @return
+	 * @throws SQLException
+	 */
+	List<Code> codeList(Search search) throws SQLException;
+	
 }
