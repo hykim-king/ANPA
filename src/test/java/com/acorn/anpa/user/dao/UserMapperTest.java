69297d9d705139ecf98694e781aa9f918fe4a56d
H.E.MOON <ansgkdms7@gmail.com>
2024-08-05 17:44:02 +0900

    240805 create

---
+++ src/test/java/com/acorn/anpa/user/dao/UserMapperTest.java
@@ -0,0 +1,23 @@
+package com.acorn.anpa.user.dao;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.FixMethodOrder;
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.acorn.anpa.cmn.PLog;
+import com.acorn.anpa.mapper.UserMapper;
+import com.acorn.anpa.member.domain.Member;
+
+public class UserMapperTest {
+
+}
