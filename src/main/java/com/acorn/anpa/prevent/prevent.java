610568a7895a0a62ca784c36df3d4a0862f20579
박수연 <167058068+bksy0506@users.noreply.github.com>
2024-08-06 12:49:34 +0900

    20240806_sy

---
+++ src/main/java/com/acorn/anpa/prevent/prevent.java
@@ -0,0 +1,67 @@
+package com.acorn.anpa.prevent;
+
+import com.acorn.anpa.cmn.DTO;
+
+public class prevent extends DTO {
+    private String preventSeq ;
+    private String title      ;
+    private int    readCnt    ;
+    private String contents   ;
+    private String imgSrc     ;
+
+  
+    
+    public prevent() {}
+    public prevent(String preventSeq, String title, int readCnt, String contents, String imgSrc, String modDt,
+            String regId, String regDt, String modId) {
+        super();
+        this.preventSeq = preventSeq;
+        this.title = title;
+        this.readCnt = readCnt;
+        this.contents = contents;
+        this.imgSrc = imgSrc;
+        
+    }
+    public String getPreventSeq() {
+        return preventSeq;
+    }
+    public void setPreventSeq(String preventSeq) {
+        this.preventSeq = preventSeq;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public int getReadCnt() {
+        return readCnt;
+    }
+    public void setReadCnt(int readCnt) {
+        this.readCnt = readCnt;
+    }
+    public String getContents() {
+        return contents;
+    }
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
+    public String getImgSrc() {
+        return imgSrc;
+    }
+    public void setImgSrc(String imgSrc) {
+        this.imgSrc = imgSrc;
+    }
+    
+    
+    @Override
+    public String toString() {
+        return "prevent [preventSeq=" + preventSeq + ", title=" + title + ", readCnt=" + readCnt + ", contents="
+                + contents + ", imgSrc=" + imgSrc 
+                 + ", toString()=" + super.toString() + "]";
+    }
+   
+    
+    
+    
+}
